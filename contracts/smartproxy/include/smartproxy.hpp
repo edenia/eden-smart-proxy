e6e61946bb6ad7aad7baf4a09246d05cf555ca5b
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-09-10 23:14:24 -0600

    perf(smartproxy): start update_votes action

--- contracts/smartproxy/include/smartproxy.hpp
+++ contracts/smartproxy/include/smartproxy.hpp
@@ -7,20 +7,56 @@ namespace edenproxy {
 
   bool is_bp_whitelisted( eosio::name bp ) { return true; }
 
-  struct smartproxy {
+  // TODO: optimize this calculation
+  uint16_t fib( uint8_t n ) {
+    uint16_t j = 1, k = 1;
+    uint16_t temp = 0;
+
+    for ( uint8_t i = 2; i < n; i++ ) {
+      temp = j + k;
+
+      j = k;
+      k = temp;
+    }
+
+    return k;
+  }
+
+  struct votes {
     eosio::name account;
     eosio::name bp;
+    uint16_t    weight;
 
     uint64_t primary_key() const { return account.value; }
+    uint64_t by_bp() const { return bp.value; }
   };
-  EOSIO_REFLECT( smartproxy, account, bp )
-  typedef eosio::multi_index< "smartproxy"_n, smartproxy > smartproxy_table;
+  EOSIO_REFLECT( votes, account, bp, weight )
+  typedef eosio::multi_index<
+      "votes"_n,
+      votes,
+      eosio::indexed_by<
+          "bybp"_n,
+          eosio::const_mem_fun< votes, uint64_t, &votes::by_bp > > >
+      votes_table;
+
+  struct stats {
+    eosio::name bp;
+    uint16_t    weight;
+
+    uint64_t primary_key() const { return bp.value; }
+  };
+  EOSIO_REFLECT( stats, bp, weight )
+  typedef eosio::multi_index< "stats"_n, stats > stats_table;
 
   struct smartproxy_contract : public eosio::contract {
   public:
     using eosio::contract::contract;
 
     void vote( eosio::name voter, eosio::name bp );
+    void removevote( eosio::name voter );
+    void proxyvote();
+
+    void update_votes();
 
   private:
     const eosio::name DEFAULT_ACCOUNT = eosio::name( "smartproxy" );
@@ -28,6 +64,8 @@ namespace edenproxy {
 
   EOSIO_ACTIONS( smartproxy_contract,
                  "smartproxy"_n,
-                 action( vote, voter, bp ) )
+                 action( vote, voter, bp ),
+                 action( removevote, voter ),
+                 action( proxyvote ) )
 
 } // namespace edenproxy
--- contracts/smartproxy/src/smartproxy.cpp
+++ contracts/smartproxy/src/smartproxy.cpp
@@ -16,29 +16,92 @@ namespace edenproxy {
     //               "Eden member rank should be greater or equal to 1" );
     eosio::check( is_bp_whitelisted( bp ), "Only whitelisted bps are allowed" );
 
-    // hc == ch
+    // hc == ch same weight
     //
+    uint8_t  rank = 1;
+    uint16_t vote_weight = fib( rank );
 
-    smartproxy_table _smartproxy{ get_self(), get_self().value };
-    auto             smartproxy_itr = _smartproxy.find( voter.value );
+    votes_table _votes{ get_self(), get_self().value };
+    auto        votes_itr = _votes.find( voter.value );
 
-    if ( smartproxy_itr == _smartproxy.end() ) {
-      _smartproxy.emplace( get_self(), [&]( auto &row ) {
+    if ( votes_itr == _votes.end() ) {
+      _votes.emplace( get_self(), [&]( auto &row ) {
         row.account = voter;
         row.bp = bp;
+        row.weight = vote_weight;
       } );
     } else {
-      _smartproxy.modify( smartproxy_itr, get_self(), [&]( auto &row ) {
+      _votes.modify( votes_itr, eosio::same_payer, [&]( auto &row ) {
         row.bp = bp;
+        row.weight = vote_weight;
+      } );
+    }
+
+    stats_table _stats{ get_self(), get_self().value };
+    auto        stats_itr = _stats.find( bp.value );
+
+    if ( stats_itr == _stats.end() ) {
+      _stats.emplace( get_self(),
+                      [&]( auto &row ) { row.weight += vote_weight; } );
+    } else {
+      _stats.modify( stats_itr, eosio::same_payer, [&]( auto &row ) {
+        row.bp = bp;
+        row.weight = vote_weight;
       } );
     }
   }
 
-  // vote
-  // remove
+  void smartproxy_contract::removevote( eosio::name voter ) {
+    require_auth( voter );
+
+    votes_table _votes{ get_self(), get_self().value };
+    auto        votes_itr = _votes.find( voter.value );
+
+    eosio::check( votes_itr != _votes.end(), "Voter does not exist" );
+
+    stats_table _stats{ get_self(), get_self().value };
+    auto        stats_itr = _stats.find( votes_itr->bp.value );
+
+    // TODO: could be less than 0 ?
+    if ( votes_itr->weight - stats_itr->weight <= 0 ) {
+      _stats.erase( stats_itr );
+    } else {
+      _stats.modify( stats_itr, eosio::same_payer, [&]( auto &row ) {
+        row.weight -= votes_itr->weight;
+      } );
+    }
+
+    _votes.erase( votes_itr );
+
+    update_votes();
+  }
+
+  void smartproxy_contract::proxyvote() {
+    require_auth( get_self() );
+
+    update_votes();
+  }
+
+  void smartproxy_contract::update_votes() {
+    // first  = lowest
+    // back   = highest
+    std::vector< std::pair< eosio::name, uint16_t > > bps;
+
+    stats_table _stats{ get_self(), get_self().value };
+
+    for ( auto itr = _stats.begin(); itr != _stats.end(); itr++ ) {
+      uint16_t distance = !bps.empty() ? bps.front() - bps.back() : 0;
+
+      // itr->weight
+    }
+
+    // vote for that bps
+  }
+
 } // namespace edenproxy
 
 EOSIO_ACTION_DISPATCHER( edenproxy::actions )
 
 EOSIO_ABIGEN( actions( edenproxy::actions ),
-              table( "smartproxy"_n, edenproxy::smartproxy ) )
+              table( "votes"_n, edenproxy::votes ),
+              table( "stats"_n, edenproxy::stats ) )
