6b5aac9f0b7ddd9b43c030b9ec3a06a6db3122da
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-10-05 13:55:17 -0600

    feat(webapp): get members templates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as routeUtils } from './route'
 export { default as analyticsUtils } from './analytics'
 export * as smartProxyUtil from './smart-proxy'
+export * as atomicAssetsUtil from './atomicassets'
