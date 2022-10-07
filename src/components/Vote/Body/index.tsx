3840c661486a26413430dd5fff8017fa682426f8
Angelo CG <angelocas13_8@hotmail.com>
2022-10-07 16:38:02 -0600

    feat(pages): create vote page

--- src/components/Vote/Body/index.tsx
+++ src/components/Vote/Body/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BlockProducerItem } from '@edenia/ui-kit'
 import { Link, Typography } from '@mui/material'
 import Image from 'next/image'
