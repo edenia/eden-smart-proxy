3fb1a52569ba66dbffe368071eb8ab6b488c6f9c
Angelo CG <angelocas13_8@hotmail.com>
2022-10-09 21:08:30 -0600

    feat(pages): load vote state in voters page

--- src/components/Voters/Body/index.tsx
+++ src/components/Voters/Body/index.tsx
@@ -1,18 +1,23 @@
+import CircularProgress from '@mui/material/CircularProgress'
 import { DelegateItem, Button } from '@edenia/ui-kit'
 import { Link, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 import { smartProxyUtil, atomicAssetsUtil, genesisEdenUtil } from 'utils'
+import yesVotingIcon from '/public/icons/yes-voting-icon.png'
+import notVotingIcon from '/public/icons/not-voting-icon.png'
 import telegramIcon from '/public/icons/telegram-grey-icon.png'
 
 import useStyles from './styles'
 
 const Body: React.FC = () => {
   const classes = useStyles()
+  const [loadingData, setLoadingData] = useState<boolean>(true)
   const [edenMembers, setEdenMembers] = useState<any>([])
 
   const loadMembers = async nextKey => {
+    setLoadingData(true)
     const members = await smartProxyUtil.getEdenMembers(
       nextKey === '' ? undefined : nextKey,
       5
@@ -20,48 +25,59 @@ const Body: React.FC = () => {
     if (members) {
       const infoMembers = await atomicAssetsUtil.getTemplates()
       const electionRankSize = await genesisEdenUtil.getRanks()
-      const membersCompleteData = members?.rows?.map((member): any => {
+      const membersCompleteDataPromise = members?.rows?.map(async member => {
         const info = infoMembers.find(
           (template): any => member[1]?.account === template?.account
         )
+        const { rows } = await smartProxyUtil.getVotes(member[1]?.account, 1)
+
         return {
           ...member,
           info: {
             ...info,
             rank: genesisEdenUtil.classifyMemberRank(
               member[1].election_rank,
-              electionRankSize.length
+              electionRankSize.length - 1
             )
           },
-          next_key: members.next_key
+          next_key: members.next_key,
+          vote: rows.length > 0 ? rows[0].producers : undefined
         }
       })
+      const membersCompleteData = await Promise?.all(membersCompleteDataPromise)
       setEdenMembers([...edenMembers, ...membersCompleteData])
+      setLoadingData(false)
     }
   }
 
   useEffect(() => {
     loadMembers(undefined)
   }, [])
-
+  console.log({ edenMembers })
   return (
     <div className={classes.container}>
       {edenMembers?.map(delegate => (
         <DelegateItem
           key={delegate[1].name}
-          text='Not Voting'
+          text={
+            delegate.vote
+              ? `Voted for ${delegate?.vote?.length} `
+              : 'Not Voting '
+          }
           name={delegate[1].name}
           image={`https://ipfs.io/ipfs/${delegate?.info?.image}`}
           target='_blank'
           avatarIcon={delegate?.info?.rank?.badge}
-          headItem={<Image src={telegramIcon} />}
-          linkIcon='/icons/ref-icon.png'
+          headItem={
+            <Image src={delegate.vote ? yesVotingIcon : notVotingIcon} />
+          }
+          linkIcon={delegate.vote ? '/icons/ref-icon.png' : undefined}
           positionText={`${delegate?.info?.rank?.label} - Rate: n`}
           selectableItems={
             <div className={classes.centerSelectableItems}>
               <Image src={telegramIcon} alt='Telegram icon' />
               <Typography
-                variant='subtitle1'
+                variant='subtitle2'
                 className={classes.labelSelectedItems}
               >
                 <Link
@@ -77,6 +93,11 @@ const Body: React.FC = () => {
           }
         />
       ))}
+      {loadingData && (
+        <div className={classes.loadMoreContainer}>
+          <CircularProgress />
+        </div>
+      )}
       {edenMembers[edenMembers?.length - 1]?.next_key !== '' && (
         <div className={classes.loadMoreContainer}>
           <Button
