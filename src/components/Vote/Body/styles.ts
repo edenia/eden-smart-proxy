32f00cdeb04b2f7d0edc3bc8b16bcf8827d8d9e9
Teto Gomez <terencio17@rocketmail.com>
2022-10-09 21:49:18 -0600

    fix(webapp): add BP item breakpoint for mobile

--- src/components/Vote/Body/styles.ts
+++ src/components/Vote/Body/styles.ts
@@ -3,13 +3,27 @@ import { makeStyles } from '@mui/styles'
 const Styles = makeStyles(theme => ({
   container: {
     padding: theme.spacing(3),
-    width: '100%'
+    width: '100%',
+    '& .delegate-bp-item-container': {
+      flexDirection: 'column',
+      padding: '8px !important',
+      alignItems: 'flex-start',
+      width: 300,
+      '& .centerItems': {
+        justifyContent: 'flex-start',
+        '& .delegate-bp-item-proxy-padding': {
+          padding: '0 41px !important'
+        }
+      }
+    }
   },
   linkPadding: {
     padding: '0 4px'
   },
   flex: {
-    display: 'flex'
+    paddingTop: '8px',
+    display: 'flex',
+    flexDirection: 'column'
   }
 }))
 
