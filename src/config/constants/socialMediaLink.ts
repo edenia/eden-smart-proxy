aab2479171963111f8b57e6acbbc51db8c9335fe
Angelo CG <angelocas13_8@hotmail.com>
2022-10-11 09:21:38 -0600

    feat(pages): vote section loading if user don't login

--- src/config/constants/socialMediaLink.ts
+++ src/config/constants/socialMediaLink.ts
@@ -5,7 +5,9 @@ const links = {
   steemit: 'https://steemit.com/',
   telegram: 'https://t.me/',
   twitter: 'https://twitter.com/',
-  wechat: 'https://www.wechat.com/'
+  wechat: 'https://www.wechat.com/',
+  youtube: 'https://www.youtube.com/',
+  keybase: 'https://keybase.io/'
 }
 
 export default { links }
