c4e89fb3e0da6460def8c2f8918ba39e6da1a803
Teto Gomez <terencio17@rocketmail.com>
2022-10-13 15:47:57 -0600

    fix(webapp): remove wallet provider menu dropdown

--- src/components/AuthUAL/index.tsx
+++ src/components/AuthUAL/index.tsx
@@ -1,65 +1,23 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import { Box, Menu } from '@mui/material'
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
 import { Button } from '@edenia/ui-kit'
 
 import useAuthUAL from './useAuthUAL.hook'
 import useStyles from './styles'
 
 const AuthButton: React.FC<{ btnLabel: string }> = ({ btnLabel }) => {
-  const [{ state }, { handleOpenMenu, handleCloseMenu, login }] = useAuthUAL()
+  const [{ state }, { login }] = useAuthUAL()
   const classes = useStyles()
 
   return (
-    <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        {!state?.ual?.activeUser && (
-          <Button onClick={handleOpenMenu} label={btnLabel} variant='primary' />
-        )}
-      </Box>
-      <Menu
-        className={classes.menuBox}
-        anchorEl={state?.elemRef || null}
-        id='account-menu'
-        open={state?.openMenuWallets}
-        onClose={handleCloseMenu}
-        onClick={handleCloseMenu}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        {(state?.ual?.authenticators || []).map(wallet => {
-          const style = wallet.getStyle()
-          const name = wallet.getName()
-
-          return (
-            <div
-              key={style.text}
-              className={classes.menuItem}
-              style={{
-                backgroundColor: style.background,
-                color: style.textColor
-              }}
-              onClick={() => login(name)}
-              role='button'
-              tabIndex={0}
-              aria-hidden='true'
-            >
-              <div className={classes.iconText}>
-                <img
-                  src={style.icon}
-                  width={28}
-                  height={28}
-                  alt='wallet indicator'
-                />
-                {style.text}
-              </div>
-              <KeyboardArrowRightIcon />
-            </div>
-          )
-        })}
-      </Menu>
-    </React.Fragment>
+    <div className={classes.loginBtn}>
+      {!state?.ual?.activeUser && (
+        <Button
+          onClick={() => login('anchor')}
+          label={btnLabel}
+          variant='primary'
+        />
+      )}
+    </div>
   )
 }
 
