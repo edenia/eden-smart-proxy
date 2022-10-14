653e290291babed1f3ef7e850964a4448e9e781a
Angelo CG <angelocas13_8@hotmail.com>
2022-10-14 10:46:55 -0600

    feat(components): add total eos votes delegate on header app

--- src/utils/eosio.ts
+++ src/utils/eosio.ts
@@ -14,3 +14,17 @@ export const hasVoteForProxy = async (account: string): Promise<boolean> => {
 
   return rows.length && rows[0].proxy === sdkConfig.edenSmartProxyContract
 }
+
+export const getTotalEosVoteDelegate = async (): Promise<number> => {
+  const { rows } = await eosApi.getTableRows({
+    code: sdkConfig.eosioContract,
+    scope: sdkConfig.eosioContract,
+    table: 'voters',
+    lower_bound: sdkConfig.edenSmartProxyContract,
+    uppper_bound: sdkConfig.edenSmartProxyContract,
+    json: true,
+    limit: 1
+  })
+
+  return rows?.staked || 0
+}
