8d9144cffdedb44f1964bec80933b905c25c4efa
Teto Gomez <terencio17@rocketmail.com>
2022-10-17 21:33:18 -0600

    fix(webapp): fix twitter card for mobile version and styling footer

--- src/components/Layout/Footer/styles.ts
+++ src/components/Layout/Footer/styles.ts
@@ -1,14 +1,27 @@
 import { makeStyles } from '@mui/styles'
 
 const Styles = makeStyles(theme => ({
-  root: {
+  footerRoot: {
     width: '100%',
-    backgroundColor: theme.palette.grey[600]
+    backgroundColor: theme.palette.grey[600],
+    '& .footer-item-style': {
+      color: 'rgba(163,163,163,1) !important',
+      fontSize: '16px !important',
+      margin: '2px 0'
+    },
+    '& .footer-item-title': {
+      letterSpacing: '.1em',
+      fontSize: '.875rem',
+      lineHeight: '1.25rem'
+    }
   },
   footerContainer: {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    '& .MuiTypography-caption': {
+      fontSize: '16px !important'
+    }
   },
   paddinR: {
     paddingRight: theme.spacing(0.5)
